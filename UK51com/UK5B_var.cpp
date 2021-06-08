5b16e3ce4b2f5c169ce750aadadf046bee546e7a
Chibis61 <chibisov61@outlook.com>
2021-06-08 15:54:16 +0800

    08.06.2021 день

--- UK51com/UK5B_var.cpp
+++ UK51com/UK5B_var.cpp
@@ -8,48 +8,28 @@
 
 #include "UK5B_var.h"
 
-UK5B_var::UK5B_var(const std::string& n)
+uk5_b_var::uk5_b_var(const std::string& n, const std::string& t, const std::string& def, const double delta)
 {
 	name_ = n;
+	type_ = m_type.at(t);
+	
+	place_ = 2;
+	std::string s = def;
+	
 	const bool x = boost::filesystem::exists("config.ini");
-
 	if (x)
 	{
 		try
 			{
 				boost::property_tree::ptree pt;
 				read_ini("config.ini", pt);
-				place_ = 2;
 				for (boost::property_tree::ptree::iterator pos = pt.begin(); pos != pt.end(); ++pos)
 				{
 					auto f = pos->second.find(name_);
 					if (f != pos->second.not_found())
 					{
 						place_ = m_place[pos->first];
-						const auto s = (*f).second.get_value<std::string>();
-						int c = 0;
-						if (s.find('.')) c += 1;
-						if (s.find(';')) c += 2;
-						switch (c)
-						{
-						case 0:								// int				счетчик 
-							type_ = 0;							
-								
-							break;
-						case 1:								// double			величина
-							type_ = 1;
-							
-							break;
-						case 2:								// vector<int>		размеры в ячейках
-							type_ = 2;
-							
-							break;
-						case 3:								// vector<double>	размеры в метрах
-							type_ = 2;
-							
-							break;
-						default: ;
-						}
+						s = (*f).second.get_value<std::string>();
 						break;
 					}
 				}
@@ -58,42 +38,132 @@ UK5B_var::UK5B_var(const std::string& n)
 		catch (const boost::property_tree::ptree_error &e)
 			{
 				f_err_.open("error.log", std::ios::out);
-				f_err_ << e.what() << std::endl;
+				f_err_ << "РћС€РёР±РєР°: " << e.what() << std::endl;
 			}
 
 	}
-	else
-	{
-		place_ = 2;
-	}
+	set_value(s, delta);
 }
 
-std::string UK5B_var::get_name() const
+std::string uk5_b_var::get_name() const
 {
 	return name_;
 }
 
-int UK5B_var::get_type() const
+int uk5_b_var::get_type() const
 {
 	return type_;
 }
 
-void UK5B_var::set_place(const int p)
+void uk5_b_var::set_place(const int p)
 {
 	place_ = p;
 }
 
-int UK5B_var::get_place() const
+int uk5_b_var::get_place() const
 {
 	return place_;
 }
 
-void UK5B_var::set_init(bool b)
+void uk5_b_var::set_init(bool b)
 {
 	init_ = b;
 }
 
-bool UK5B_var::is_init() const
+bool uk5_b_var::is_init() const
 {
 	return init_;
+}
+
+void uk5_b_var::set_value(const std::string& def, double delta)
+{
+	int c = 0;
+	if (boost::algorithm::contains(def, ".")) c += 1;
+	if (boost::algorithm::contains(def, ";")) c += 2;
+	const auto s = (def.empty()) ? "0" : def;
+	boost::char_separator<char> sep(";");
+	boost::tokenizer< boost::char_separator<char> > list(s, sep);
+	try
+	{
+		switch (c)
+		{
+		case 0:								// int				СЃС‡РµС‚С‡РёРє 
+			((place_ != 1) ? value_d_.first : value_d_.second) = stod(s);
+			break;
+		case 1:								// double			РІРµР»РёС‡РёРЅР°
+			((place_ != 1) ? value_i_.first : value_i_.second) = stoi(s);
+			break;
+		case 2:								// vector<int>		СЂР°Р·РјРµСЂС‹ РІ СЏС‡РµР№РєР°С…
+			int end_i = 1;
+			for (auto& itr : list)
+			{
+					if (boost::algorithm::contains(itr, ":"))
+					{
+						boost::trim_if(itr, is_brackets);
+						boost::char_separator<char> sep2(":");
+						boost::tokenizer< boost::char_separator<char> > list2(itr, sep2);
+						auto it = list2.begin();
+						const int first = (std::stoi(*it) == 0) ? end_i : std::stoi(*it);
+						const int cc = (std::stoi(*(++it)) == 0) ? 1 : std::stoi(*(++it));
+						const int step = (std::distance(list2.begin(), list2.end()) == 2) ? 1 : ((std::stoi(*(++it)) == 0) ? 1 : std::stoi(*(++it)));
+						for (int i = 1; i <= cc; i += 1) 
+							((place_ != 1) ? vector_i_.first : vector_i_.second).push_back(first + ((i - 1) * step));
+					}
+					else
+						((place_ != 1) ? vector_i_.first : vector_i_.second).push_back(stoi(itr));
+					end_i = ((place_ != 1) ? vector_i_.first : vector_i_.second).back();
+			}
+			(place_ != 1) ? value_i_.first = static_cast<int>(vector_i_.first.size()) : value_i_.second = static_cast<int>(vector_i_.second.size());
+			break;
+		case 3:								// vector<double>	СЂР°Р·РјРµСЂС‹ РІ РјРµС‚СЂР°С…
+			double end_d = 0.;
+			for (auto& itr : list)
+			{
+					if (boost::algorithm::contains(itr, ":"))
+					{
+						boost::trim_if(itr, is_brackets);
+						boost::char_separator<char> sep2(":");
+						boost::tokenizer< boost::char_separator<char> > list2(itr, sep2);
+						auto it = list2.begin();
+						const double first = (std::stod(*it) == 0.) ? end_d : std::stod(*it);
+						const int cc = (std::stoi(*(++it)) == 0) ? 1 : std::stoi(*(++it));
+						const double step = (std::distance(list2.begin(), list2.end()) == 2) ? delta : ((std::stod(*(++it)) == 0.) ? delta : std::stod(*(++it)));
+						for (int i = 1; i <= cc; i += 1)
+						{ 
+							double tmp = first + ((i - 1) * step);
+							((place_ != 1) ? vector_d_.first : vector_d_.second).push_back(tmp);
+							((place_ != 1) ? vector_i_.first : vector_i_.second).push_back(static_cast<int>(std::round(tmp / delta)));
+						}
+					}
+					else
+					{
+						((place_ != 1) ? vector_d_.first : vector_d_.second).push_back(stod(itr));
+						((place_ != 1) ? vector_i_.first : vector_i_.second).push_back(static_cast<int>(std::round(stod(itr) / delta)));
+						
+					}
+					end_d = ((place_ != 1) ? vector_d_.first : vector_d_.second).back();
+			}
+			(place_ != 1) ? value_i_.first = static_cast<int>(vector_d_.first.size()) : value_i_.second = static_cast<int>(vector_d_.second.size());
+			break;
+		default: ;
+		}
+	}
+	catch (const std::exception& e)
+	{
+		f_err_.open("error.log", std::ios::out);
+		f_err_ << "РћС€РёР±РєР°: " << e.what() << std::endl;
+	}
+
+}
+
+std::string uk5_b_var::get_value()
+{
+	std::string s = "0";
+	
+	return s;
+}
+
+bool uk5_b_var::is_brackets(const char c)
+{
+	return c == '(' || c == ')';
 }
--- UK51com/UK5B_var.h
+++ UK51com/UK5B_var.h
@@ -5,26 +5,31 @@
 #include <map>
 #include <fstream>
 
-class UK5B_var
+class uk5_b_var
 {
 
 public:
-	explicit UK5B_var(const std::string&);										//	конструктор 
-	~UK5B_var() = default;														//	деструктор 
-//	UK5B_var(const UK5B_var&) = default;										//	конструктор копирования
-//	UK5B_var(UK5B_var&&)  noexcept = default;									//	конструктор переноса
-//	UK5B_var& operator = (const UK5B_var&) = default;							//	оператор копирования
-//	UK5B_var& operator = (UK5B_var&&) noexcept = default;						//	оператор переноса
+//	uk5_b_var() = default;																											//  конструктор по умолчанию
+explicit uk5_b_var(const std::string& n, const std::string& t, const std::string& def="0.", double delta = 1.0);					//	конструктор (пользовательский) 
+//	~uk5_b_var() = default;																											//	деструктор 
+// 	uk5_b_var(const uk5_b_var&) = default;																							//	конструктор копирования
+//	uk5_b_var(uk5_b_var&&)  noexcept = default;																						//	конструктор присваивания
+//	uk5_b_var& operator = (const uk5_b_var&) = default;																				//	оператор копирования
+//	uk5_b_var& operator = (uk5_b_var&&) noexcept = default;																			//	оператор присваивания
 
-	std::string get_name() const;
-	int get_type() const;
+std::string get_name() const;
+		int get_type() const;
 	
-	void set_place(int);
-	int get_place() const;
+  	   void set_place(int);
+		int	get_place() const;
 
-	void set_init(bool);
-	bool is_init() const;
-	
+	   void set_init(bool);
+	   bool is_init() const;
+
+	   void set_value(const std::string& def="0.", double delta = 1.0);
+static std::string get_value();
+static bool is_brackets(char c);
+
 	std::map<std::string,int>							m_place = {{"in",0},{"in_out",1},{"out",2},{"final",3}};
 	std::map<std::string,int>							m_type = {{"quantity",0},{"quality",1},{"geometry",2}};
 
@@ -35,9 +40,9 @@ private:
 	bool												init_ = false;
 	
 	std::pair<double, double>							value_d_ = { 0.,0. };
-	std::pair<std::vector<double>, std::vector<double>>	vector_d_ = { {},{} };
+	std::pair<std::vector<double>, std::vector<double>>	vector_d_ = { {0.},{0.} };
 	std::pair<int, int>									value_i_ = {0,0};
-	std::pair<std::vector<int>, std::vector<int>>		vector_i_ = { {},{} };
+	std::pair<std::vector<int>, std::vector<int>>		vector_i_ = { {0},{0} };
 
 	std::ofstream										f_err_;
 
